55b9e7630a65785ea40d649c8d333b9e82fa3268
AliHasanIbrahim47 <ew9361903@gmail.com>
2023-12-09 12:06:32 -0500

    first comment

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+
+import AvilableMeals from "./AvailableMeals";
+import MealsSummary from "./MealsSummary";
+
+const Meals = () => {
+    return (
+        <Fragment>
+            <MealsSummary />
+            <AvilableMeals />
+        </Fragment>
+    )
+};
+
+export default Meals;
